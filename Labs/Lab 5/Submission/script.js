7224eb6503de345ebea871a2e553be8cac383e23
kyliewasserman <79178779+kyliewasserman@users.noreply.github.com>
2021-10-06 12:41:43 -0400

    lab 5 through js

--- Labs/Lab 5/Submission/script.js
+++ Labs/Lab 5/Submission/script.js
@@ -1,1 +1,26 @@
-document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByClassName('title')[0].innerHTML='New Text Changed through Java Script';
+//h1 text change
+document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByClassName('title')[0].innerHTML='Lab 5';
+
+//description change to name
+document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('p')[1].innerHTML='Kylie Wasserman';
+
+//make the text less bossy
+document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('h3')[0].innerHTML='Pretty please do these things ... :(';
+
+//change the last li to say the name of the class
+document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('ul')[0].getElementsByTagName('li')[4].innerHTML='Software Dev 2';
+
+//make the first li to be blue
+const zero = document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('ul')[0].getElementsByTagName('li')[0];
+zero.style.color = "blue";
+
+//make the second li to be a background color of cream (cream isn't a set option so I did beige)
+const one = document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('ul')[0].getElementsByTagName('li')[1];
+one.style.backgroundColor = "beige";
+
+//change the background of the whole thing to grey
+document.getElementsByTagName('body')[0].style.background = "grey";
+
+//make the fourth li item all uppercase
+const three = document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('ul')[0].getElementsByTagName('li')[3];
+three.style.textTransform = "uppercase";
